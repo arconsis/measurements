c32e475b42f5cff074c063ff03fbd01cb1aa5f78
Christian Navolskyi <christian.navolskyi@arconsis.com>
2020-08-06 18:02:22 +0200

    added ppi for iPads

--- ios/Classes/SwiftMeasurePlugin.swift
+++ ios/Classes/SwiftMeasurePlugin.swift
@@ -17,8 +17,12 @@ public class SwiftMeasurePlugin: NSObject, FlutterPlugin {
                 ppi = 458.0
             case "iPhone 6 Plus", "iPhone 6s Plus", "iPhone 7 Plus", "iPhone 8 Plus":
                 ppi = 401.0
-            case "iPhone 4", "iPhone 4s", "iPhone 5", "iPhone 5s", "iPhone 5c", "iPhone SE (1st generation)", "iPhone 6", "iPhone 6s", "iPhone 7", "iPhone 8", "iPhone 11", "iPhone Xʀ", "iPhone SE (2nd generation)":
-                fallthrough
+            case "iPhone 4s", "iPhone 5", "iPhone 5s", "iPhone 5c", "iPhone SE (1st generation)", "iPhone 6", "iPhone 6s", "iPhone 7", "iPhone 8", "iPhone 11", "iPhone Xʀ", "iPhone SE (2nd generation)", "iPad mini 2", "iPad mini 3", "iPad mini 4", "iPad mini (5th generation)":
+                ppi = 326.0
+            case "iPad Retina", "iPad Air", "iPad Air 2", "iPad Pro (9.7-inch)", "iPad Pro (12.9-inch)", "iPad (5th generation)", "iPad Pro (12.9-inch) (2nd generation)", "iPad (10.5-inch)", "iPad (6th generation)", "iPad (7th generation)", "iPad Pro (11-inch) (1st generation)", "iPad Pro (12.9-inch) (3rd generation)", "iPad Pro (11-inch) (2nd generation)", "iPad Pro (12.9-inch) (4th generation)", "iPad Air (3rd generation)":
+                ppi = 264.0
+            case "iPad 2":
+                ppi = 132
             default:
                 ppi = 326.0
             }
